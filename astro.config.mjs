9e58d77789bb5acdcc1198d3b0e0444a6ac313fd
danlakelake <danlakelake@gmail.com>
2026-05-06 22:56:34 -0600

    Ignore VSCode launch settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+// WordPress Environment Variables
+const WORDPRESS_URL = import.meta.env.PUBLIC_WORDPRESS_API;
+const WORDPRESS_DOMAIN = WORDPRESS_URL ? new URL(WORDPRESS_URL).hostname : '';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   image: {
-    domains: ['coffeeshop.local'],
+    domains: [WORDPRESS_DOMAIN],
   },
 });
